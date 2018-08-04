4af781174d50415d1ff73dd4e84bab11bda15815
Hugo Greyvenstein <bones.greyvenstein@gmail.com>
2018-08-04 15:49:08 +0200

    Initial

---
+++ src/main/java/com/game/marblepits/MarblePitsApplication.java
@@ -0,0 +1,14 @@
+package com.game.marblepits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarblePitsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MarblePitsApplication.class, args);
+    }
+}
