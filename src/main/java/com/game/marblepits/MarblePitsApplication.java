0ade7fcf03868b47c3fc8a77c530b70701fcd92c
Hugo Greyvenstein <bones.greyvenstein@gmail.com>
2018-08-05 12:09:53 +0200

    Basic game display

--- src/main/java/com/game/marblepits/MarblePitsApplication.java
+++ src/main/java/com/game/marblepits/MarblePitsApplication.java
@@ -2,6 +2,9 @@ package com.game.marblepits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class MarblePitsApplication
@@ -10,5 +13,13 @@ public class MarblePitsApplication
     public static void main(String[] args)
     {
         SpringApplication.run(MarblePitsApplication.class, args);
+    }
+
+    @Configuration
+    @Profile("local")
+    @ComponentScan(lazyInit = true)
+    static class LocalConfig
+    {
+
     }
 }
--- src/main/java/com/game/marblepits/controllers/MainController.java
+++ src/main/java/com/game/marblepits/controllers/MainController.java
@@ -1,13 +1,27 @@
 package com.game.marblepits.controllers;
 
+import com.game.marblepits.engine.GameEngine;
+import com.game.marblepits.entities.Board;
+import com.game.marblepits.repositories.BoardDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 
 @Controller
 public class MainController
 {
+    private GameEngine gameEngine;
+
+    private BoardDao boardDao;
+
+    public MainController(GameEngine gameEngine, BoardDao boardDao)
+    {
+        this.gameEngine = gameEngine;
+        this.boardDao = boardDao;
+    }
+
     @GetMapping("")
     public String index(Model model)
     {
@@ -17,6 +31,16 @@ public class MainController
     @PostMapping("/start-game")
     public String startGame(Model model)
     {
+        model.addAttribute("board", gameEngine.newGame());
+        return "game";
+    }
+
+    @GetMapping("/game/{id}")
+    public String findGame(@RequestPart String id, Model model) throws Exception
+    {
+        Board board = boardDao.findById(Long.parseLong(id)).orElseThrow(() -> new Exception("not found"));
+        model.addAttribute("board", board);
+
         return "game";
     }
 }
